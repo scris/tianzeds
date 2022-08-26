0d3a6825dd3e7ab9185a2d5f2f2a45a987c9ef5f
Tianze Ds Qiu <slaaker@126.com>
2022-08-26 15:35:28 +0800

    projects info

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
         Tianze Ds
       </div>
       <div class="line desc">
-        Enthusiast for creating, and building great apps that're benificial to
+        Enthusiast for creating things, and building great apps that're benificial to
         people's lives.
       </div>
       <div class="line education">
@@ -31,28 +31,73 @@
         <div class="kind">
           <span class="less-use"
             >HTML, CSS, Vanilla JS, Pytorch, Git, Photoshop, Unity, Premiere
-            Pro, QT, Capacitor ...</span
-          >
+            Pro, QT, Capacitor, Lightroom ...</span>
         </div>
       </div>
     </div>
     <div class="section section-flex">
       <div class="section-title">Projects&nbsp;</div>
       <div class="section-content">
-        <div class="kind">
-          - Mr. Noplay&nbsp;<span class="tech">during Grade 10</span>
+        <div id="focam">
+          <div class="kind">
+            - Focam, Plannium&nbsp;<span class="tech">Grade 12 - freshman year</span>
+          </div>
+          <div class="line-desc kind-desc">
+            &nbsp;&nbsp;Using Flutter and Electron, under pre-alpha development. </div>
         </div>
-        <div class="line-desc kind-desc">
-          &nbsp;&nbsp;Made with Electron. Give an end to no-end rests and play times. With a extra-strict way to work to keep users on the schedule, and a vivid user interface.
+        <div id="lika" class="line-below">
+          <div class="kind">
+            - Lika AI Work&nbsp;<span class="tech">before freshman year</span>
+          </div>
+          <div class="line-desc kind-desc">
+            &nbsp;&nbsp;Using Pytorch. Built the software part of a low-cost low-image-resolution fabric anomaly detection system with another 12th-grader on top of Maix-II and STFPM. </div>
         </div>
-        <div class="line-desc tech">
-          &nbsp;&nbsp;<a href="https://github.com/mrnoplay/mrnoplay" target="_blank">[GitHub]</a>&nbsp;<a href="https://mrnoplay.tianzeds.com/" target="_blank">[Website]</a>
+        <div id="timona" class="line-below">
+          <div class="kind">
+            - Timona&nbsp;<span class="tech">during Grade 11-12</span>
+          </div>
+          <div class="line-desc kind-desc">
+            &nbsp;&nbsp;Using Flutter, in private beta. One of the strongest ways to keep you on schedule, and live fully of your self-planned times.</div>
+          <div class="line-desc tech">
+            &nbsp;&nbsp;<a href="https://s2.loli.net/2022/08/26/Mo7zSU9KWhnYylP.png" target="_blank">[Screenshot]</a>
+          </div>
+        </div>
+        <div id="mrnoplay" class="line-below">
+          <div class="kind">
+            - Mr. Noplay&nbsp;<span class="tech">during Grade 10</span>
+          </div>
+          <div class="line-desc kind-desc">
+            &nbsp;&nbsp;Using Electron. Give an end to no-end rests and play times. With a extra-strict way to work to keep users on the schedule, and a vivid user interface.
+          </div>
+          <div class="line-desc tech">
+            &nbsp;&nbsp;<a href="https://github.com/mrnoplay/mrnoplay" target="_blank">[GitHub]</a>&nbsp;<a href="https://mrnoplay.tianzeds.com/" target="_blank">[Website]</a>&nbsp;<a href="https://i.loli.net/2020/02/09/XewHqoUimbFtPRO.jpg" target="_blank">[Older Version]</a>
+          </div>
+        </div>
+        <div id="switchable" class="line-below">
+          <div class="kind">
+            - Switchable&nbsp;<span class="tech">during Grade 9</span>
+          </div>
+          <div class="line-desc kind-desc">
+            &nbsp;&nbsp;Using Electron, archived. A plan-based alarm, allowing you to create amount of daily plans, then quickly switch from one to another.</div>
+          <div class="line-desc tech">
+            &nbsp;&nbsp;<a href="https://github.com/scris/switchable" target="_blank">[GitHub]</a>&nbsp;<a href="https://s2.loli.net/2022/08/26/UkWZjidsT5ol3cA.png" target="_blank">[Screenshot]</a>
+          </div>
+        </div>
+        <div id="mixbank" class="line-below">
+          <div class="kind">
+            - Mixbank&nbsp;<span class="tech">during Grade 8</span>
+          </div>
+          <div class="line-desc kind-desc">
+            &nbsp;&nbsp;Using Unity. Try your best to click faster in order to get a terrific score!</div>
+          <div class="line-desc tech">
+            &nbsp;&nbsp;<a href="https://i.loli.net/2018/02/26/5a9371bf6d192.jpeg" target="_blank">[Screenshot]</a>
+          </div>
         </div>
       </div>
     </div>
     <div class="section">
       <div class="section-content shallow">
-        Copyright &copy; Tianze Ds Qiu 2022. GitHub @scris
+        Copyright &copy; Tianze Ds Qiu 2022. GitHub <a href="https://github.com/scris" target="_blank">@scris</a>.
       </div>
     </div>
   </div>
@@ -86,7 +131,7 @@ body {
   background-color: #fdfdfd;
   color: $black;
   font-weight: 500;
-  font-family: "Spot Mono", monospace;
+  font-family: "", monospace;
   line-height: 110%;
 }
 
