7d96baeb61ce89ad018326ddf1bb32559da684c0
Tianze Ds Qiu <slaaker@126.com>
2025-12-07 23:55:00 +0800

    education and fact update
    - update the gitignore file

--- src/main.tsx
+++ src/main.tsx
@@ -12,16 +12,16 @@ const Main: React.FC = () => {
         <div className={'normal'}>
           <p>
             Undergraduate Student of both&nbsp;
-            <a href={'https://www.bupt.edu.cn/'}>BUPT</a>
+            <a href={'https://www.bupt.edu.cn/'} target={'_blank'}>BUPT</a>
             &nbsp;and&nbsp;
-            <a href={'https://www.qmul.ac.uk/'}>Queen Mary</a>,
+            <a href={'https://www.qmul.ac.uk/'} target={'_blank'}>Queen Mary</a>,
             doing research on Agentic AI, NLP and AI Safety.
           </p>
           <p>
             Incoming Ph.D. student of both&nbsp;
-            <a href={'https://cs.fudan.edu.cn/'}>College of CS and AI, Fudan University</a>
+            <a href={'https://cs.fudan.edu.cn/'} target={'_blank'}>College of CS and AI, Fudan University</a>
             &nbsp;and&nbsp;
-            <a href={'https://sii.edu.cn/'}>Shanghai Innovation Institute</a>.
+            <a href={'https://sii.edu.cn/'} target={'_blank'}>Shanghai Innovation Institute</a>.
           </p>
           <p>Also got interests in Apps / Web Development. Loving Cross-Platform.</p>
         </div>
@@ -30,6 +30,7 @@ const Main: React.FC = () => {
         <div className={'subtitle'}>Education</div>
         <div className={'normal'}>
           <p><span className={'time'}>Starting from 2026.9:</span> Fudan University, Ph.D. student in Computer Science and Technology.</p>
+          <p><span className={'time'}>Starting from 2026.1:</span> UPV, València, Spain, Exchange student at ETSIT, UPV.</p>
           <p><span className={'time'}>2022.9 - 2026.6:</span> BUPT, Undergraduate student in Intelligent Science and Technology. Rank: 2 / 98.</p>
           <p><span className={'time'}>2019.9 - 2022.6:</span> Hangzhou No.2 High School, Zhejiang.</p>
         </div>
@@ -45,6 +46,7 @@ const Main: React.FC = () => {
       <div className={'part'}>
         <div className={'subtitle'}>Noteworthy Facts</div>
         <div className={'normal'}>
+          <p><span className={'time'}>2025.10:</span> As the first student author and second author, I had a paper, <a href={'https://www.researchgate.net/publication/396418425_WebCloak_Characterizing_and_Mitigating_Threats_from_LLM-Driven_Web_Agents_as_Intelligent_Scrapers'} target={'_blank'}>WebCloak</a>, accepted at IEEE S&amp;P '26 (Security Big 4, CCF-A, Core-A*, Acceptance Rate 118 / 925 ≈ 13%).</p>
           <p><span className={'time'}>2024.12:</span> I was awarded the 2024 China National Scholarship.</p>
           <p><span className={'time'}>2024.10:</span> I won International Gold Medal (Top 0.009%) at China International College Students' Innovation Competition (a.k.a. “Internet+”) as leading contributor.</p>
           <p><span className={'time'}>2024.2:</span> The project I co-led, MashOn, is listed among 18 typical cases of AI + Higher Education by the Ministry of Education of China.</p>
